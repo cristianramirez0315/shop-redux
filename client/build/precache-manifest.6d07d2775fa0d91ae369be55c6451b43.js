28b2472127b50190bfef24175a8164d509b25640
Cristian Ramirez <cristianramirez0315@gmail.com>
2022-02-02 03:31:13 -0500

    first commit

---
+++ client/build/precache-manifest.6d07d2775fa0d91ae369be55c6451b43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "305d930a2d4694ed9175fbd0668803ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca466c7159f7a72a88bb",
+    "url": "/static/css/main.e3b24089.chunk.css"
+  },
+  {
+    "revision": "bb57aac40e5f09fa75e5",
+    "url": "/static/js/2.e685f201.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.e685f201.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca466c7159f7a72a88bb",
+    "url": "/static/js/main.1c065619.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
